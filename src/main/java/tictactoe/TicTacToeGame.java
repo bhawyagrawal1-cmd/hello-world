93549eff1919a3ba8bb89bf02439aa5bc8000c79
bhawyagrawal1-cmd <bhawyagrawal1@gmail.com>
2026-04-20 21:57:49 +0530

    Coded Tic-Tac-Toe UC2: Toss player and symbol

--- src/main/java/tictactoe/TicTacToeGame.java
+++ src/main/java/tictactoe/TicTacToeGame.java
@@ -1,11 +1,17 @@
 package tictactoe;
 
+import java.util.Random;
+
 public class TicTacToeGame {
     public static void main(String[] args) {
         char[][] board = new char[3][3];
+        Random random = new Random();
 
         initializeBoard(board);
+        GameState gameState = tossToStart(random);
+
         printBoard(board);
+        printGameState(gameState);
     }
 
     static void initializeBoard(char[][] board) {
@@ -28,6 +34,35 @@ public class TicTacToeGame {
                 }
             }
             System.out.println();
+        }
+    }
+
+    static GameState tossToStart(Random random) {
+        boolean humanStarts = random.nextBoolean();
+
+        String currentPlayer = humanStarts ? "Human" : "Computer";
+        char humanSymbol = humanStarts ? 'X' : 'O';
+        char computerSymbol = humanStarts ? 'O' : 'X';
+
+        return new GameState(currentPlayer, humanSymbol, computerSymbol);
+    }
+
+    static void printGameState(GameState gameState) {
+        System.out.println("Toss Result:");
+        System.out.println("Human Symbol: " + gameState.humanSymbol);
+        System.out.println("Computer Symbol: " + gameState.computerSymbol);
+        System.out.println("First Turn: " + gameState.currentPlayer);
+    }
+
+    static class GameState {
+        String currentPlayer;
+        char humanSymbol;
+        char computerSymbol;
+
+        GameState(String currentPlayer, char humanSymbol, char computerSymbol) {
+            this.currentPlayer = currentPlayer;
+            this.humanSymbol = humanSymbol;
+            this.computerSymbol = computerSymbol;
         }
     }
 }
